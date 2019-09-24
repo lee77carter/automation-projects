767b2b874234478075f1b75966d2dea7b9398553
Leandra C <lee77carter@users.noreply.github.com>
2019-09-24 10:26:41 -0700

    Updated and renamed file

--- cloneTarShipUntar.bash
+++ cloneTarShipUntar.bash
@@ -10,11 +10,21 @@ stagingDir=/usr/lee77carter/doNotBackup
 targetDir=/usr/lee77carter/build/doNotBackup
 HOST=lee77carter.gogurl.com
 tarFile=lee77carter.tar
+version=r_1.1.SP1_HF8
+tarDir=cooldirectory
 
 # Create Tarball #
-sudo -E -u lee77carter /bin/tar cvf $stagingDir/$tarFile ../cooldirectory
+sudo -E -u lee77carter /bin/tar cvf $stagingDir/$tarFile ../$tarDir
 
 # Ship Tarball to Old Server #
 echo "Transferring tarball to old server"
 sudo -E -u lee77carter scp $targetDir/$tarFile $HOST:/usr/lee77carter/build/doNotBackup/.
 echo "Transfer Complete"
+     				
+# Unpack & run build: 
+cd $stagingDir
+/bin/tar xvf $tarFile
+/bin/mv $tarDir $version
+cd $version
+
+# Now log in and run those manual commands on that old pesky system
