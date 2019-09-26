cee10262965a1b0fb226c45e912b5342fde03e81
Leandra C <lee77carter@users.noreply.github.com>
2019-09-26 12:06:57 -0700

    Updated and added add'tl parameters

--- jiraCreation.bash
+++ jiraCreation.bash
@@ -7,47 +7,55 @@
 set -e
 echo "III_RLSENG=$JIRAPASSWORD"
 
+## Exceptions to the rule for InnReach
+if [ "${PRODUCT}" = "Lee-Lee" ]; then
+  PRODUCT_RELEASE_BRANCH_NAME="LeeLeeSharing"
+  PRODUCT_TAG="lees"
+else
+  PRODUCT_RELEASE_BRANCH_NAME="${PRODUCT}"
+  PRODUCT_TAG="${PRODUCT,,}" ## lower case
+fi
 
-## Bundle Dashboard Jira's - Team dashboard for tragging versions##
+## Bundle Dashboard Jira's ##
 if [ "${BUND_DASHBOARD}" = true ]; then
 
-        curl \
+        curl -sS \
         -X POST \
         -u $JIRAPASSWORD \
         -H "Content-Type: application/json" \
-        https://<yourURL>/rest/api/2/issue \
-        -d '{ "fields": { "project": { "key":"BUND" }, "summary":"UpdateMe 9.9.9", "issuetype": { "name":"Bundle" }, "customfield_11240" : ["'UpdateMe_9.9.9_1'"], "customfield_11345" : ["'9.9.9_1'"] } }'
+        https://jira.iii.com/rest/api/2/issue \
+        -d '{ "fields": { "project": { "key":"BUND" }, "summary":"'"${PRODUCT} ${VERSION}_${UNDERBAR_NUMBER}"'", "issuetype": { "name":"Bundle" }, "customfield_11240" : ["'"${PRODUCT_TAG}_${VERSION}_${UNDERBAR_NUMBER}"'"], "customfield_11345" : ["'"${VERSION}_${UNDERBAR_NUMBER}"'"] } }'
 fi
 
-## Building Jira's - for adding subtasks and entire project completion notes##
+## Building Jira's ##
 if [ "${BUILDING_JIRA}" = true ]; then
 
         curl -sS \
         -X POST \
         -u $JIRAPASSWORD \
         -H "Content-Type: application/json" \
-        https://<yourURL>/rest/api/2/issue \
-        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Building UpdateMe_9.9.9", "issuetype": { "name":"Task"} } }'
+        https://jira.iii.com/rest/api/2/issue \
+        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Building '"${PRODUCT}_${VERSION}_${UNDERBAR_NUMBER}"'", "issuetype": { "name":"Task"} } }'
 fi 
 
-## Merging Develop Jira's - merge to dev after ever RC release##
+## Merging Develop Jira's ##
 if [ "${MERGE_JIRA_DEVELOP}" = true ]; then
 
         curl -sS \
         -X POST \
         -u $JIRAPASSWORD \
         -H "Content-Type: application/json" \
-        https://<yourURL>/rest/api/2/issue \
-        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Merging UpdateMe release/UpdateMe_9.9.9 to Develop", "issuetype": { "name":"Task"} } }'
+        https://jira.iii.com/rest/api/2/issue \
+        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Merging '"${PRODUCT} release/${PRODUCT}_${VERSION}"' to Develop", "issuetype": { "name":"Task"} } }'
 fi
 
-## Merging Master Jira's - tracks merge to GA and deletion of branches once in GA##
+## Merging Develop Jira's ##
 if [ "${MERGE_JIRA_MASTER}" = true ]; then 
 
         curl -sS \
         -X POST \
         -u $JIRAPASSWORD \
         -H "Content-Type: application/json" \
-        https://<yourURL>/rest/api/2/issue \
-        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Merging UpdateMe release/UpdateMe_9.9.9 into Master and Delete Release Branches", "issuetype": { "name":"Task"} } }'
+        https://jira.iii.com/rest/api/2/issue \
+        -d '{ "fields": { "project": { "key":"REL" }, "summary":"Merging '"${PRODUCT} release/${PRODUCT}_${VERSION}"' into Master and Delete Release Branches", "issuetype": { "name":"Task"} } }'
 fi 
